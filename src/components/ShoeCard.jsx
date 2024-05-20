5631847695d03d2b1aee856c55a8c24ae0bc647a
s3xmaxman <badside3919@gmail.com>
2024-05-20 17:19:02 +0900

    complete Shoe Card

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
-const ShoeCard = () => {
+const ShoeCard = ({ imgUrl, changeBigShoeImage, bigShoeImg }) => {
+  
+  const handleClick = () => {
+    if( bigShoeImg !== imgUrl.bigShoe ) {
+        changeBigShoeImage(imgUrl.bigShoe)
+    }
+  }
+
   return (
-    <div>ShoeCard</div>
+    <div 
+        className={`border-2 rounded-xl ${bigShoeImg === imgUrl.bigShoe ? 'border-coral-red' : 'border-transparent'} cursor-pointer  max-sm:flex-1`} 
+        onClick={handleClick}
+    >
+      <div className='flex justify-center items-center bg-card bg-center bg-cover rounded-xl max-sm:p-4'>
+        <img 
+            src={imgUrl.thumbnail} 
+            alt="shoe collection"
+            width={127}
+            height={103}
+            className='object-contain' 
+        />
+      </div>  
+    </div>
   )
 }
 
